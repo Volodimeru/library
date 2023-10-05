ab1b368c6276c848f988cc360222d745461f6533
volodimeru <vladi.dogonadze@gmail.com>
2023-10-05 14:14:42 +0300

    Final Version before storage

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function Book(title,author,pages,read) {
     this.pages = pages
     this.read = read
 }
+
 Book.prototype.toggleRead = function() {
     this.read = !this.read;
 }
@@ -25,7 +26,7 @@ function render() {
         <p>${book.read ? "Read" : "Not read yet"}</p>
         <p id="doublebut">
         <button id="toggleBtn" class="btn" onclick="toggleRead(${i})">Change status</button>
-        <button class="btn" onclick="removeBook(${i})">Remove</button>
+        <button id="removeBtn" class="btn" onclick="removeBook(${i})">Remove book</button>
         </p>
         </div>
         `;
@@ -45,6 +46,7 @@ function removeBook(index) {
     }
     render();
 }
+
 function addBookToLibrary() {
     let title = document.querySelector('#title').value;
     let author = document.querySelector("#author").value;
@@ -54,45 +56,42 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
     render();
 }
+
 const contentContainer = document.querySelector(".contentContainer");
 const content = document.querySelector(".content");
 const addButtonContainer = document.querySelector(".addButtonContainer");
 const newBookContainer = document.querySelector('.newBookContainer');
-const addNewBook = document.querySelector('#addNewBook');
-const cancel = document.querySelector("#cancel");
+const addBookBtn = document.querySelector('#addNewBook');
 const submitBtn = document.querySelector("#submitBtn");
 
-addNewBook.addEventListener("click", function(event) {
+
+addBookBtn.addEventListener("click", function(event) {
     event.preventDefault();
     addButtonContainer.style.display = "none";
     newBookContainer.style.display = "block";
 })
 
-document.querySelector("#cancelBtn").addEventListener("click", function (event) {
+cancelBtn.addEventListener("click", function (event) {
     event.preventDefault();
     title.value = "";
     author.value = "";
     pages.value = "";
     read.checked = false;
-
     newBookContainer.style.display = "none";
     addButtonContainer.style.display = "block";
 })
 
 submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
-    if (title.value != "" &&
-        author.value != ""
-    ) {
+    if (title.value != "" && author.value != "") {
         newBookContainer.style.display = "none";
         addButtonContainer.style.display = "block";
         contentContainer.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    content.style.display = "block";
-    addBookToLibrary()
-    }
+        content.style.display = "block";
+        addBookToLibrary()
+        }
     title.value = "";
     author.value = "";
     pages.value = "";
     read.checked = false;
 })
-
